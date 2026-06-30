530d9e496a180ccfea8b22d5fbca2f97660841ea
Saif Ullah <ranasaifullahkhalid2@gmail.com>
2026-06-30 08:44:54 +0500

    fixing api key for deployment

--- src/llm.py
+++ src/llm.py
@@ -2,24 +2,29 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import streamlit as st
 import os
+from pathlib import Path
 
-# Load .env for local development
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / ".env"
 
-# Try Streamlit Secrets first
-try:              
+print("Env path:", env_path)
+print("Exists:", env_path.exists())
+
+load_dotenv(dotenv_path=env_path)
+
+try:
     api_key = st.secrets["GROQ_API_KEY"]
+    print("Using Streamlit Secret")
 except Exception:
     api_key = os.getenv("GROQ_API_KEY")
+    print("Using .env")
+
+print("API Key:", api_key)
 
 if not api_key:
-    raise ValueError(
-        "GROQ_API_KEY not found. Add it to Streamlit Secrets or your local .env file."
-    )
+    raise ValueError("API Key not found")
 
 llm = ChatGroq(
     api_key=api_key,
     model="qwen/qwen3-32b",
     temperature=0.7,
-    max_tokens=1024,
 )
